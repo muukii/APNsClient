373a52d4a5fb301e20faf431a3d2d59b94da77f6
Muukii <muukii.app@gmail.com>
2019-11-21 07:12:38 +0900

    Initial Commit

---
+++ APNsClient/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  APNsClient
+//
+//  Created by muukii on 2019/11/21.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
